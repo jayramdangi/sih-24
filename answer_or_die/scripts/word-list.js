aa05838717eba140e01f31de01a9395b8334d397
Jayram Dangi <128122311+jayramdangi@users.noreply.github.com>
2024-08-30 07:35:17 +0530

    Add files via upload

---
+++ answer_or_die/scripts/word-list.js
@@ -0,0 +1,31 @@
+const wordList = [
+    {
+        word: "president",
+        hint: "which rule is implemented in state emergency ?"
+    },
+    {
+        word: "president",
+        hint: "Who makes rules for union territories ?"
+    },
+    
+    {
+        word: "Cabinet",
+        hint: "The President can proclaim emergency on the written advice of the union ____"
+    },
+    {
+        word: "Vicepresident",
+        hint: "The President gives his resignation to the ? "
+    },
+    {
+        word: "Primeminister",
+        hint: "The Governor of a state is appointed by the President on the advice of the ?"
+    },
+    {
+        word: "six",
+        hint: "Incase if President dies while in office, the vice President can act as a president for how  many months ?"
+    },
+    {
+        word: "Parliament",
+        hint: "The President of India can be removed by ? "
+    },
+];
